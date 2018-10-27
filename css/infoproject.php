017ea8bf4f5d244a786de340bc7a4fe35e8a901b
sherrylive98 <36100742+sherrylive98@users.noreply.github.com>
2018-10-27 08:07:36 +0530

    Add files via upload

---
+++ css/infoproject.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Info-Project</title>
+</head>
+<body>
+
+	<div class="name">Name</div>
+	<div class="addmember">MEMBER LIST</div>
+	<div class="description">Description</div>
+	<div class="todolist">ToDo Task</div>
+	<div class="addskill">Add Skills</div>
+	<div class="bugs">BUGS</div>
+	<div class="progressgraph">progress</div>
+</body>
+</html>
